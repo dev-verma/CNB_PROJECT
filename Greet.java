041d51ac60e2461d221f6942559806057a9f3f11
dev-verma <vermakqr@gmail.com>
2023-03-01 22:37:19 +0530

    now m1(){} completed

--- Greet.java
+++ Greet.java
@@ -1,7 +1,8 @@
 classs Greet{
 int i=10;
 m1(){
-System.out.println("hi good morning");
+System.out.println("hi good morning guys.");
+System.out.println("Learn DevOps");
 }
 m2(){
 s.o.p("devops");
