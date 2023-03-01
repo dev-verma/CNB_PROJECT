4d26fc8ba912c4ed49968b3955955b994d7c537d
dev-verma <vermakqr@gmail.com>
2023-03-01 23:01:55 +0530

    now m1() completed

--- demo.java
+++ demo.java
@@ -1,8 +1,15 @@
 class Demo{
 int c=20;
+<<<<<<< Updated upstream
 m2(){
 System.out.println("I know this is demo class");
 System.out.println("Thanks for updating....");
 }
 
+=======
+m1(){
+S.o.p("this is demo class");
+S.o.p("value of c is "+c);
+}
+>>>>>>> Stashed changes
 }
